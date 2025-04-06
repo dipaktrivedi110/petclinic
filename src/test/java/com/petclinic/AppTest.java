2ff22534551b4f9c818668d24cc9add4bcbc7de5
dipaktrivedi110 <87696788+dipaktrivedi110@users.noreply.github.com>
2025-04-06 16:39:35 +0530

    Update AppTest.java

--- src/test/java/com/petclinic/AppTest.java
+++ src/test/java/com/petclinic/AppTest.java
@@ -1,12 +1,11 @@
 package com.petclinic;
 
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
     @Test
     public void testApp() {
-        assertTrue(true); // Always passes
+        assertTrue(true);
     }
 }
-
